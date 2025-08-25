2b354445e3d138a5e33a72ae55f05ac51a420598
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-08-25 19:37:50 -0300

    user screen props receive

--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from "react-native"
+import { View, Text } from "react-native";
+//resgatar valor da rota
+import { RouteProp } from "@react-navigation/native";
+import { RootStackParamList } from "../../App";
 
-export default function UserScreen() {
+type UserScreenProps = {
+    route: RouteProp<RootStackParamList, 'User'>
+}
+
+export default function UserScreen({ route }: UserScreenProps) {
+    const { username } = route.params
+
     return (
         <View>
-            <Text>User Works</Text>
+            <Text>Bem vindo(a): {username}</Text>
         </View>
     )
 }
