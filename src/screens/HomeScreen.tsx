3553e0b5ad0ee18a73492f73be925cff84cd5f57
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-08-25 19:30:49 -0300

    home screen props

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,33 @@
-import { View, Text } from "react-native"
+import { useState } from 'react'
+import { View, Text, TextInput, Button } from "react-native";
 
-export default function HomeScreen() {
+//criar propriedade para o navegador
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../../App';
+
+type HomeScreenProps = {
+    navigation: StackNavigationProp<RootStackParamList, 'Home'>
+}
+
+export default function HomeScreen({ navigation }: HomeScreenProps) {
+
+    const [inputText, setInputText] = useState('');
+
+    const navigateToUserScreen = () => {
+        navigation.navigate('User', { username: inputText });
+    }
+
     return (
         <View>
-            <Text>Home Works</Text>
+            <TextInput
+                placeholder="Digite seu nome..."
+                value={inputText}
+                onChangeText={(text) => setInputText(text)}
+            />
+            <Button
+                title='Logar'
+                onPress={navigateToUserScreen}
+            />
         </View>
     )
 }
