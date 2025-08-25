58bf85858970df4099c86b7f953871ea58dcb0c6
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-08-25 19:19:56 -0300

    navigation

--- App.tsx
+++ App.tsx
@@ -1,15 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
 import HomeScreen from './src/screens/HomeScreen';
 import UserScreen from './src/screens/UserScreen';
 
+export type RootStackParamList = {
+  Home: undefined;
+  User: { username: string }
+}
+
+const { Navigator, Screen } = createStackNavigator<RootStackParamList>();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator>
+        <Screen name='Home' component={HomeScreen} />
+        <Screen name='User' component={UserScreen} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
 
